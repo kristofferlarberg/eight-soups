3ce9da505a0aa4f12815a2165dba591d9bb609f3
kristofferlarberg <mail@kristofferlarberg.se>
2020-10-06 16:44:41 +0200

    firebase setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Eight Soups
 
 An online delivery web app built with React and Firebase. WIP.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,45 @@
+import app from "firebase/app";
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase
+
+/*   <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.22.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.22.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDDH0ahbfYc2FIm76cwizTLTyfr5AqznlI",
+    authDomain: "eight-soups.firebaseapp.com",
+    databaseURL: "https://eight-soups.firebaseio.com",
+    projectId: "eight-soups",
+    storageBucket: "eight-soups.appspot.com",
+    messagingSenderId: "1052602479925",
+    appId: "1:1052602479925:web:cde8a7348a21e1eacf887c",
+    measurementId: "G-1TY8YYWWNM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script> */
